417aa43503715bc12e75c90387fa1b0c104e4dec
Conradgabe <gisuekebho5880@gmail.com>
2022-11-03 11:09:14 +0000

    function to take the values of the absolute url of he csv file

---
+++ csv_json1.py
@@ -0,0 +1,27 @@
+import csv
+import json
+
+def csv_to_json(csv_file_path, json_file_path):
+    # create a dict
+    data_dict = {}
+
+    # open a csv file 
+    with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
+        csvreader = csv.DictReader(csv_file)
+
+        # convert each row into a dictionary and adding to the data_dict variable
+        for rows in csvreader:
+            key = rows['Serial Number']
+            data_dict[key] = rows
+
+    # open a json file
+    with open(json_file_path, 'w', encoding='utf-8') as json_file:
+        json_obj = json.dumps(data_dict, indent=4)
+        json_file.write(json_obj)
+
+csv_file_path = input('Enter the absolute path of the CSV file: ')
+json_file_path = input('Enter the absolute path of the JSON file: ')
+
+print('working on it...')
+
+csv_to_json(csv_file_path, json_file_path)
